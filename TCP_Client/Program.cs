05411ae2c7817067cbce4185bb999d69241eec03
Betti-spaghetti <101730869+b3ths1@users.noreply.github.com>
2024-09-26 21:10:20 +0300

    fin_Update Program.cs

--- TCP_Client/Program.cs
+++ TCP_Client/Program.cs
@@ -5,8 +5,8 @@ using System.Threading;
 
 class Client
 {
-    private const int Port = 8888;
-    private const string ServerIp = "127.0.0.1";
+    private const int Port = 8888; //задава постоянен номер за порта на сървъра
+    private const string ServerIp = "127.0.0.1"; //задава IP-то на сървъра
 
     static void Main()
     {
@@ -15,39 +15,40 @@ class Client
 
         NetworkStream stream = client.GetStream(); //Връща NetworkStream, използван за изпращане и получаване на данни.
 
-        Thread receiveThread = new Thread(ReceiveMessages);
+        //Thread се използва за обработка на съобщения от сървъра.
+        Thread receiveThread = new Thread(ReceiveMessages); 
         receiveThread.Start(stream);
 
-        while (true)
+        while (true) //безкраен цикъл (докато не се даде знак да спре)
         {
-            string message = Console.ReadLine();
-            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            string message = Console.ReadLine(); //чете съобщението, написано от клиента
+            byte[] buffer = Encoding.ASCII.GetBytes(message); //превръща съобщението в масив от ASCII символи/с ASCII encoding
             stream.Write(buffer, 0, buffer.Length);
         }
     }
 
-    static void ReceiveMessages(object obj)
+    static void ReceiveMessages(object obj) //този метод се използва при Thread-овете горе ↑
     {
         NetworkStream stream = (NetworkStream)obj;
-        byte[] buffer = new byte[1024];
-        int bytesRead;
+        byte[] buffer = new byte[1024]; //инициализира се буфер, който запазва данните на съобщението
+        int bytesRead; //променлива, която следи за прочетените байтове
 
         while (true)
         {
-            try
+            try //try-catch, за да се предпази програмата от грешки или нещо такова :')
             {
-                bytesRead = stream.Read(buffer, 0, buffer.Length);
-                if (bytesRead == 0)
+                bytesRead = stream.Read(buffer, 0, buffer.Length); //чете данни в буфера и връща броя на прочетените байтове
+                if (bytesRead == 0) //ако няма прочетени байтове/съобщение, да се прекъсне цикъла
                 {
                     break;
                 }
 
-                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Console.WriteLine(message);
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead); //обратно - превръща масива с байтове в string
+                Console.WriteLine(message); //съобщението излиза
             }
-            catch (Exception)
+            catch (Exception) //ако има грешки, да се прекъсне цикъла
             {
-                break;
+                break; 
             }
         }
     }
